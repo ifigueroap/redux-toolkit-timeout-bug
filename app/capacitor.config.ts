f3a22ea1f1e0f3e91da2312b2e3853030038b69d
Ismael Figueroa <ifigueroap@gmail.com>
2023-09-11 18:57:09 -0300

    WIP

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -1,9 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'cl.itrend.condor',
-  appName: 'ItrendCL Cóndor',
-  webDir: 'build',
+  appId: "com.myapp.app",
+  appName: "My App",
+  webDir: "build",
   bundledWebRuntime: false,
 };
 
